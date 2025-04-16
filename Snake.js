a86295ae2ca69b063542f6ad4cdb7adb7a013d9d
schoolman12 <802454@sylvaniastudents.org>
2025-04-16 12:45:18 -0400

    working on it pt. 3

--- Snake.js
+++ Snake.js
@@ -15,7 +15,7 @@ function drawSnake() {
 }
 
 function drawFood() {
-    ctx.fillStyle ="red";
+    ctx.fillStyle = "red";
     ctx.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
 
 }
@@ -27,8 +27,70 @@ function moveSnake() {
         case "up":
             head.y--;
             break;
-            case "down":
-                head.y--;
-                
+        case "down":
+            head.y++;
+            break;
+        case "left":
+            head.x--;
+            break;
+        case "right":
+            head.x++;
+            break;
+
+    }
+
+    snake.unshift(head);
+
+    if (head.x === food.x && head.y === food.y) {
+        generateFood();
+    } else {
+        snake.pop();
     }
 }
+
+function generateFood() {
+    food = {
+        x: Math.floor(Math.random() * gridSize),
+        y: Math.floor(Math.random() * gridSize)
+    };
+}
+
+function checkCollisions() {
+    const head = snake[0];
+
+    if (head.x < 0 || head.x >= gridSize || head.y < 0 || head.y < 0 || head.y >= gridSize) {
+        resetGame();
+    }
+
+    for (let i = 1; i < snake.length; i++) {
+        if (head.x === snake[i].x && head.y === snake[i].y) {
+            resetGame();
+        }
+    }
+}
+
+function resetGame() {
+    alert("Game over! Restarting...");
+    snake = [{ x: 5, y: 5 }];
+    direction = "right";
+    generateFood();
+}
+
+function updateGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake();
+    drawFood();
+    moveSnake();
+    checkCollisions();
+}
+
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowUp":
+            direction = "up";
+            break;
+        case "ArrowDown":
+            direction = "down";
+            break;
+        case "ArrowLeft"
+    }
